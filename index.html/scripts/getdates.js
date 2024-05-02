1b423a3f616fea0699a2e5b4a11fef5d2c13e586
MAHONRRIMENDEZ <97697808+MAHONRRIMENDEZ@users.noreply.github.com>
2024-05-01 23:45:42 -0600

    Completed

--- index.html/scripts/getdates.js
+++ index.html/scripts/getdates.js
@@ -1,1 +1,10 @@
+function dateTime() {
+    var dateTime = new Date();
+    var date = dateTime.toLocaleDateString();
+    var time = dateTime.toLocaleTimeString();
 
+    var dateTimeString = "Date: " + date + " Time: " + time;
+    
+    document.getElementById("dateTime").innerText = dateTimeString;
+}
+setInterval(dateTime, 1000);
