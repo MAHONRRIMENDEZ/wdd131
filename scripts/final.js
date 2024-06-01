affa6da9da32ed809822b4aaf0b45583de54a526
MAHONRRIMENDEZ <97697808+MAHONRRIMENDEZ@users.noreply.github.com>
2024-05-31 21:24:42 -0600

    comp

---
+++ scripts/final.js
@@ -0,0 +1,22 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+function getCurrentYear() {
+    const currentYearElement = document.getElementById('currentyear');
+    const currentYear = new Date().getFullYear();
+    currentYearElement.textContent = currentYear;
+}
+
+function getLastModifiedDate() {
+    const lastModifiedElement = document.getElementById('lastModified');
+    const lastModified = new Date(document.lastModified);
+    const formattedDate = `${lastModified.getDate()}/${lastModified.getMonth() + 1}/${lastModified.getFullYear()} ${lastModified.getHours()}:${lastModified.getMinutes().toString().padStart(2, '0')}`;
+    lastModifiedElement.textContent = `Last Modified: ${formattedDate}`;
+}
+getCurrentYear();
+getLastModifiedDate();
